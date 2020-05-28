7f8e7f0d6bc623bde7e8c65f2e2e21fc52d31bc1
Marco Jeffrey Pansa <51530201+MJPansa@users.noreply.github.com>
2020-05-28 14:25:18 +0200

    Add files via upload

--- models.py
+++ models.py
@@ -11,8 +11,9 @@ class DDPGActor(nn.Module):
 
         self.input = nn.Linear(n_states, n_hidden)
         self.l1 = nn.Linear(n_hidden, n_hidden)
-        self.l2 = nn.Linear(n_hidden, n_hidden)
         self.out = nn.Linear(n_hidden, n_actions)
+
+        self.dropout = nn.Dropout(0.5)
 
         self.optimizer = optim.SGD(self.parameters(), lr=lr)
         self.to(device)
@@ -21,10 +22,9 @@ class DDPGActor(nn.Module):
         if not isinstance(x, T.Tensor):
             x = T.Tensor(x).unsqueeze(0).to(self.device)
 
-        x = F.relu(self.input(x))
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        x = F.tanh(self.out(x))
+        x = self.dropout(F.relu(self.input(x)))
+        x = self.dropout(F.relu(self.l1(x)))
+        x = self.dropout(F.tanh(self.out(x)))
 
         return x
 
@@ -34,19 +34,19 @@ class DDPGCritic(nn.Module):
         super(DDPGCritic, self).__init__()
         self.device = device
 
-        self.input = nn.Linear(n_states, n_hidden)
-        self.l1 = nn.Linear(n_hidden + n_actions, n_hidden)
-        self.l2 = nn.Linear(n_hidden, n_hidden)
+        self.input = nn.Linear(n_states + n_actions, n_hidden)
+        self.l1 = nn.Linear(n_hidden, n_hidden)
         self.out = nn.Linear(n_hidden, 1)
+
+        self.dropout = nn.Dropout(0.5)
 
         self.optimizer = optim.SGD(self.parameters(), lr=lr)
         self.to(self.device)
 
     def forward(self, x, y):
-        x = F.relu(self.input(x))
-        x = T.cat([x, y], dim=1)
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
+        x = self.dropout(self.input(T.cat([x, y], dim=1)))
+        x = self.dropout(F.relu(self.l1(x)))
         x = self.out(x)
+
 
         return x
